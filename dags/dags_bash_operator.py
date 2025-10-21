b43444770e5d8c6a95818e1f46373d7fe06c7b74
hyunseong <dnclgk9@naver.com>
2025-10-22 00:08:06 +0900

    dags_수정

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -9,9 +9,9 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.sdk import DAG
 
 with DAG(
-    dag_id="example_bash_operator",
+    dag_id="dags_bash_operator",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2025, 10, 22, tz="Asia/Seoul"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
     
